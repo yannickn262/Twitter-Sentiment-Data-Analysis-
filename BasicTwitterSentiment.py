8a2c26df42ef6e89796bac5d588365ff31f50185
Yannick Nandury <37671464+yannickn262@users.noreply.github.com>
2018-07-30 14:42:51 -0400

    Add files via upload

--- BasicTwitterSentiment.py
+++ BasicTwitterSentiment.py
@@ -1,3 +1,5 @@
+#this code is based off "Javapolcalypse's" Youtube Twitter Sentiment Analysis Tutorial
+
 from textblob import TextBlob
 import sys, tweepy
 import matplotlib.pyplot as plt
@@ -19,11 +21,45 @@ api = tweepy.API(auth)
 public_tweets = api.search('Trump')
 
 keywordSearch = input("Enter a keyword/hashtag:  ")
-noOfSearchTerms = int(input("Enter how many tweets to analyze"))
+noOfSearchTerms = int(input("Enter how many tweets to analyze: "))
 tweets = tweepy.Cursor(api.search, q = keywordSearch, lang = "English").items(noOfSearchTerms)
 #above line is responsible for creating filters as to how to narrow down search results
 
-for tweet in public_tweets:
-
+pos = 0
+neg = 0
+neutral = 0
+polarity = 0
+for tweet in tweets:
     analysis = TextBlob(tweet.text)
-    print(analysis.sentiment)
+    if(analysis.sentiment.polarity == 0):
+        neutral +=1
+    elif(analysis.sentiment.polarity > 0.00):
+        pos += 1
+    elif(analysis.sentiment.polarity < 0.00):
+        neg += 1
+pos = percentage(pos,noOfSearchTerms)
+pos = format(pos, '.2f')
+#converts sentiment to a percentage and formats to two decimal places
+neg = percentage(neg,noOfSearchTerms)
+neg = format(neg, '.2f')
+neutral = percentage(neutral,noOfSearchTerms)
+neutral = format(neutral, '.2f')
+
+print("Based off of " + str(noOfSearchTerms) + " Tweets on " + keywordSearch + ",people are ")
+if (polarity == 0):
+    print("Neutral")
+elif(polarity > 0.00):
+    print("Positive")
+elif(polarity < 0.00):
+    print("Negative")
+
+#this creates the pie chart
+labels = ['Positive ['+str(pos)+ '%]'], 'Neutral [' + str(neutral) + '%]', 'Negative [' + str(neg) +'%]'
+sizes = [pos, neutral, neg]
+colors = ['yellowgreen', 'gold', 'red']
+patches, texts = plt.pie(sizes, colors = colors, startangle = 90)
+plt.legend(patches, labels, loc = "best")
+plt.title('How people are reacting on ' + keywordSearch + ' by analyzing ' + str(noOfSearchTerms) + ' Tweets.')
+plt.axis('equal')
+plt.tight_layout()
+plt.show()
